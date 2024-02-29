127a5971da9307bbdb897303a5f266e162ea0b65
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-02-29 14:33:31 +0700

    Update branch.js

--- controllers/branch.js
+++ controllers/branch.js
@@ -102,10 +102,6 @@ exports.createBranch = async (req, res) => {
       name: Joi.string().min(1).required(),
       phone: Joi.string().pattern(/^\+62\d{9,12}$/).required(),
       address: Joi.string().required(),
-<<<<<<< Updated upstream
-      managerId: Joi.number().required(),
-=======
->>>>>>> Stashed changes
     });
     const { error, value } = schema.validate(req.body, { abortEarly: false });
 
