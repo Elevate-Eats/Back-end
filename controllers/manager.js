9d9d3ff3b275590dc67f3fa76a05e89262c86109
Muhammad Garma <mgarma76@gmail.com>
2024-03-06 11:36:49 +0700

    Docs: add for manager endpoint

--- controllers/manager.js
+++ controllers/manager.js
@@ -36,12 +36,12 @@ exports.showAllManager = async (req, res) => {
         };
       });
       if (search) {
-        branchData = branchData.filter((branch) => branch.name.toLowerCase().startsWith(
+        managerData = managerData.filter((branch) => branch.name.toLowerCase().startsWith(
           search.toLowerCase(),
         ));
       }
       if (limit) {
-        branchData = branchData.slice(0, Number(limit));
+        managerData = managerData.slice(0, Number(limit));
       }
       return res.status(200).json({
         error: false,
@@ -71,7 +71,7 @@ exports.showSingleManager = async (req, res) => {
       }
 
       const {
-        id, name, phone, email, role, branchAccess,
+        name, phone, email, role, branchAccess,
       } = results.rows[0];
       const managerData = {
         id, name, phone, email, role, branchAccess,
@@ -106,11 +106,11 @@ exports.deleteManager = async (req, res) => {
   } catch (err) {
     console.log('deleteManager Error:');
     console.log(err);
-    return res.status(500).json({ error: true, message: 'Failed deleteManager' });
+    return res.status(500).json({ error: true, message: 'Failed delete Manager' });
   }
   return console.log('deleteManager controller executed');
 };
-//Helper for createManager
+// Helper for createManager
 async function checkEmailExistence(email) {
   return new Promise((resolve, reject) => {
     db.query('SELECT email from users WHERE email = $1', [email], (err, results) => {
@@ -124,8 +124,8 @@ async function checkEmailExistence(email) {
   });
 }
 
-exports.createManager = async (req, res) =>{
-  try{
+exports.createManager = async (req, res) => {
+  try {
     const authorizationHeader = req.headers.authorization;
     const token = authorizationHeader.split(' ')[1];
     const decoded = jwt.decode(token, process.env.JWT_SECRET);
@@ -151,7 +151,7 @@ exports.createManager = async (req, res) =>{
       });
     }
     const {
-      name, nickname, phone, role, email, branchAccess, password
+      name, nickname, phone, role, email, branchAccess, password,
     } = value;
     const emailExists = await checkEmailExistence(email);
     if (emailExists) {
@@ -161,23 +161,23 @@ exports.createManager = async (req, res) =>{
       });
     }
     const hashedPassword = await bcrypt.hash(password, 8);
-    await db.query('INSERT INTO users (name, nickname, email, password, role, phone, branchAccess, companyId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [name, nickname, email, hashedPassword, role, phone, branchAccess, companyid], (err)=>{
-      if (err){
+    await db.query('INSERT INTO users (name, nickname, email, password, role, phone, branchAccess, companyId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [name, nickname, email, hashedPassword, role, phone, branchAccess, companyid], (err) => {
+      if (err) {
         console.log(err);
-      return res.status(500).json({
-        error: true,
-        message: 'failed to addManager',
-      });
-    }
+        return res.status(500).json({
+          error: true,
+          message: 'failed to addManager',
+        });
+      }
       return res.status(200).json({
         error: false,
-        message: 'Manager Added'
-      })
-    })
-  }catch (err){
-    console.log("Add Manager Error")
-    console.log(err)
-    return res.status(500).json({ error: true, message: "Failed createManager"})
+        message: 'Manager Added',
+      });
+    });
+  } catch (err) {
+    console.log('Add Manager Error');
+    console.log(err);
+    return res.status(500).json({ error: true, message: 'Failed createManager' });
   }
   return console.log('createBranch controller executed');
 };
@@ -191,7 +191,7 @@ exports.updateManager = async (req, res) => {
       phone: Joi.string().pattern(/^\+62\d{9,12}$/).required(),
       role: Joi.string().valid('general_manager', 'area_manager', 'store_manager').required(),
       email: Joi.string().email().required(),
-      branchAccess: Joi.string().required()
+      branchAccess: Joi.string().required(),
     });
 
     const { error, value } = schema.validate(req.body, { abortEarly: false });
@@ -204,7 +204,7 @@ exports.updateManager = async (req, res) => {
       });
     }
     const {
-      id, name, nickname, phone, role, email, branchAccess
+      id, name, nickname, phone, role, email, branchAccess,
     } = value;
     checkEmailExistence(email);
     db.query('UPDATE users SET name = $2, nickname = $3, phone = $4, role = $5, email = $6, branchaccess = $7 WHERE id = $1', [id, name, nickname, phone, role, email, branchAccess], (err) => {
