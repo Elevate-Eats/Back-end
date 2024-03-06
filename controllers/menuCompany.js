010528d5e252f8f8b46e38a6db47fe2335a271de
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-03-06 12:32:30 +0700

    Updating Eslint

--- controllers/menuCompany.js
+++ controllers/menuCompany.js
@@ -12,7 +12,7 @@ const db = new Pool({
 
 exports.addMenu = async (req, res) => {
   try {
-    const authorizationHeader = res.headers.authorization;
+    const authorizationHeader = req.headers.authorization;
     const token = authorizationHeader.split(' ')[1];
     const decoded = jwt.decode(token, process.env.JWT_SECRET);
     const { companyid } = decoded;
@@ -33,7 +33,7 @@ exports.addMenu = async (req, res) => {
     const {
       name, category, basePrice, baseOnlinePrice,
     } = value;
-    await db.query('INSERT INTO menus (name, category, basePrice, baseOnlinePrice, companyId) VALUES ($1, $2, $3, $4, $5)', [name, category, basePrice, baseOnlinePrice, companyid], (err) => {
+    db.query('INSERT INTO menus (name, category, basePrice, baseOnlinePrice, companyId) VALUES ($1, $2, $3, $4, $5)', [name, category, basePrice, baseOnlinePrice, companyid], (err) => {
       if (err) {
         console.log(err);
         return res.status(500).json({
@@ -52,4 +52,117 @@ exports.addMenu = async (req, res) => {
     return res.status(500).json({ error: true, message: 'Failed to add Product: Server Error' });
   }
   return console.log('addMenu Executed');
+};
+
+exports.showMenus = async (req, res) => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader.split(' ')[1];
+    const decoded = jwt.decode(token, process.env.JWT_SECRET);
+    const { companyid } = decoded;
+    const { search, limit } = req.query;
+    db.query('SELECT * FROM menus WHERE companyid = $1', [companyid], (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          error: true,
+          message: 'Failed to showMenu',
+        });
+      }
+      let menuData = result.rows.map((menu) => {
+        const {
+          id, name, category,
+        } = menu;
+        return {
+          id, name, category,
+        };
+      });
+      if (search) {
+        menuData = menuData.filter((menu) => menu.name.tolowercase().startswith(
+          search.tolowercase(),
+        ));
+      }
+      if (limit) {
+        menuData = menuData.slice(0, Number(limit));
+      }
+      return res.status(200).json({
+        error: false,
+        message: 'menuData retrieved successfully',
+        menuData,
+      });
+    });
+  } catch (err) {
+    console.log('showMenus Error');
+    console.log(err);
+    return res.status(500).json({ error: true, message: 'Failed to show Menus: Server Error' });
+  }
+  return console.log('ShowMenus Executed');
+};
+
+exports.showSingleMenu = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      id: Joi.number().min(1).required(),
+    });
+    const { error, value } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(204).json({
+        error: true,
+        message: 'Validation error',
+        details: error.details.map((x) => x.message),
+      });
+    }
+    const { id } = value;
+    db.query('SELECT * FROM menus WHERE id = $1', [id], (err, result) => {
+      if (err) {
+        console.log('showSingleMenu ERROR', err);
+        return res.status(500).json({
+          err: true,
+          message: 'Failed to retrieve menu',
+        });
+      }
+      const {
+        name, category, basePrice, baseOnlinePrice,
+      } = result.rows[0];
+      const menuData = {
+        name, category, basePrice, baseOnlinePrice,
+      };
+      return res.status(200).json({
+        error: false,
+        message: 'MenuData retrieved successfully',
+        menuData,
+      });
+    });
+  } catch (err) {
+    console.log('showMenu Error');
+    console.log(err);
+    return res.status(500).json({ error: true, message: 'Failed to show Menu: Server Error' });
+  }
+  return console.log('showSingleMenu Executed');
+};
+
+exports.updateMenu = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      name: Joi.string().min(1).required(),
+      category: Joi.string().min(1).required(),
+      basePrice: Joi.number().required(),
+      baseOnlinePrice: Joi.number().required(),
+    });
+    const { error, value } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(204).json({
+        error: true,
+        message: 'Validation error',
+        details: error.details.map((x) => x.message),
+      });
+    }
+    const { id, name, category, basePrice, baseOnlinePrice } = value;
+    db.query('UPDATE menus SET ')
+  } catch (err) {
+    console.log('updateMenu Error');
+    console.log(err);
+    return res.status(500).json({ error: true, message: 'Failed to update Menu: Server Error' });
+  }
+  return console.log('ShowMenus Executed');
 };
