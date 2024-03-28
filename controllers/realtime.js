bc5dcdd3ac0f74f0e40d11c5756cf381f5db1068
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-03-28 13:53:25 +0700

    Adding showDailyAnalytics

--- controllers/realtime.js
+++ controllers/realtime.js
@@ -36,3 +36,52 @@ exports.recordTransaction = async (req, res) => {
     return res.status(500).json({ error: true, message: 'Failed to record Transaction, Server Error' });
   }
 };
+
+exports.showdailysummary = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      branchId: Joi.number().min(1).required(),
+      startDate: Joi.date(),
+      endDate: Joi.date(),
+    });
+    const { error, value } = schema.validate(req.query, { abortEarly: false });
+    if (error) {
+      return res.status(204).json({
+        error: true,
+        message: 'Validation error',
+        details: error.details.map((x) => x.message),
+      });
+    }
+    const { branchId, startDate, endDate } = value;
+    const results = await db.query('SELECT * FROM dailysummaries WHERE branchId = $1 AND date >= $2 AND date <= $3 ORDER BY date ASC', [branchId, startDate, endDate]);
+    const data = results.rows.map((dailyAnalytics) => {
+      const {
+        id,
+        date,
+        companyid,
+        branchid,
+        totalsales,
+        numberoftransactions,
+        numberofitemssold,
+      } = dailyAnalytics;
+      return {
+        id,
+        date,
+        companyid,
+        branchid,
+        totalsales,
+        numberoftransactions,
+        numberofitemssold,
+      };
+    });
+    return res.status(200).json({
+      error: false,
+      message: 'DailyAnalytics data fetched',
+      data,
+    });
+  } catch (err) {
+    console.log('showDailyAnalytics Error');
+    console.log(err);
+    return res.status(500).json({ error: true, message: 'Failed to showDailyAnalytics, Server Error' });
+  }
+};
