6667ae764ff3f10f89c84b243aea7cc1ec2eec1f
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-05-08 23:17:14 +0700

    Update: Index Prettify
    Removing unnecessary component, moving db mechanism to ./db

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const multer = require('multer');
-const { Pool } = require('pg');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const db = require('./db/func/pool');
 
-const upload = multer();
 const app = express();
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(upload.none());
 
 // SwaggerUI Docs
 // Please Add $DOCS_URL and $DOCS_PORT for HTTP or $DOCS_HTTPS_URL for HTTPS to build ENV
@@ -48,16 +45,7 @@ app.use(
   swaggerUi.setup(swSpecs),
 );
 
-// Database Connection
-const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
-
-// Connect to the database
+// Test
 db.connect()
   .then(() => console.log('Connected to PostgreSQL'))
   .catch((err) => console.error('Error connecting to PostgreSQL:', err));
