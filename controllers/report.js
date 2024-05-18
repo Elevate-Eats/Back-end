3949b4228907de09e34ad4936b07aa6eb932e253
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2024-05-18 16:44:27 +0700

    Add: Prediction Transaction
    API for fetch prediction from microservice

--- controllers/report.js
+++ controllers/report.js
@@ -36,3 +36,44 @@ exports.getDailyReport = async (req, res) => {
     });
   }
 };
+
+exports.predictTransaction = async (req, res) => {
+  const { branchId, startDate, endDate } = req.query;
+  const apiKey = process.env.API_KEY;
+  const serviceUrl = process.env.REPORT_SERVICE_URL;
+
+  const url = new URL('/api/v1/reports/predict-transaction', serviceUrl);
+  url.searchParams.append('branchId', branchId);
+  url.searchParams.append('startDate', new Date(startDate).toISOString().split('T')[0]);
+  url.searchParams.append('endDate', new Date(endDate).toISOString().split('T')[0]);
+  console.log(url);
+  try {
+    const fetch = (await import('node-fetch')).default;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        apikey: apiKey,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    console.log(response);
+    const data = await response.json();
+
+    const dataAsArray = Array.isArray(data) ? data : [data];
+
+    return res.status(200).json({
+      success: true,
+      data: dataAsArray,
+    });
+  } catch (error) {
+    console.error('Failed to fetch transaction prediction:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Failed to fetch transaction prediction',
+    });
+  }
+};
--- routes/reports.js
+++ routes/reports.js
@@ -57,4 +57,95 @@ const router = express.Router();
  */
 router.get('/showDailyReport', isLoggedIn, reportController.getDailyReport);
 
+/**
+ * @swagger
+ * paths:
+ *   /report/v1/predictTransaction:
+ *     get:
+ *       summary: Predicts transaction counts
+ *       tags:
+ *         - Report
+ *       security:
+ *         - bearerAuth: []
+ *       parameters:
+ *         - in: query
+ *           name: branchId
+ *           schema:
+ *             type: integer
+ *           required: true
+ *           description: The ID of the branch for transaction prediction
+ *         - in: query
+ *           name: startDate
+ *           schema:
+ *             type: string
+ *             format: date-time
+ *           required: true
+ *           description: The start date for prediction in YYYY-MM-DD
+ *         - in: query
+ *           name: endDate
+ *           schema:
+ *             type: string
+ *             format: date-time
+ *           required: true
+ *           description: The end date for prediction in YYYY-MM-DD
+ *       responses:
+ *         200:
+ *           description: Transaction prediction data
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     Tanggal:
+ *                       type: string
+ *                       example: '2023-10-15'
+ *                     Shift:
+ *                       type: integer
+ *                       example: 1
+ *                     Jumlah Transaksi:
+ *                       type: integer
+ *                       example: 18
+ *                     Prev_Week_Transactions:
+ *                       type: integer
+ *                       example: 21
+ *                     Holiday:
+ *                       type: boolean
+ *                       example: false
+ *                     Weekend:
+ *                       type: boolean
+ *                       example: true
+ *                     DayType:
+ *                       type: string
+ *                       example: 'Weekend'
+ *                     Months:
+ *                       type: integer
+ *                       example: 10
+ *                     Days:
+ *                       type: integer
+ *                       example: 6
+ *                     Ramadhan:
+ *                       type: boolean
+ *                       example: false
+ *         400:
+ *           description: Bad request, parameter missing or incorrect
+ *         401:
+ *            description: Unauthorized due to Token Problem
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: object
+ *                  properties:
+ *                    error:
+ *                      type: boolean
+ *                      example: true
+ *                    message:
+ *                      type: string
+ *                      example: "Unauthorized: Token expired"
+ *         500:
+ *           description: Internal server error, unable to predict transactions
+ */
+router.get('/predictTransaction', isLoggedIn, reportController.predictTransaction);
+
 module.exports = router;
